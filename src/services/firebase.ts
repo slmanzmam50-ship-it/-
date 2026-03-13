232301825876f77546be936bab74e9fb8990375e
slmanzmam50 <slmanzmam50@example.com>
2026-03-13 14:05:04 +0300

    Update: Migrate Firebase configuration to slman-zmam project.

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCSy4P169IsP0UHK5nOFNJgMa2P2m_kIyk",
-    authDomain: "basmh-87425.firebaseapp.com",
-    projectId: "basmh-87425",
-    storageBucket: "basmh-87425.firebasestorage.app",
-    messagingSenderId: "71938062457",
-    appId: "1:71938062457:web:326afb074f75cbcab6bca8",
-    measurementId: "G-21PD7B8R4H"
+    apiKey: "AIzaSyAdTZKrSsTVIQndTRCsrRjNMydn9ITSDVY",
+    authDomain: "slman-zmam.firebaseapp.com",
+    projectId: "slman-zmam",
+    storageBucket: "slman-zmam.firebasestorage.app",
+    messagingSenderId: "750725696422",
+    appId: "1:750725696422:web:2e63633a124ad70754dd67",
+    measurementId: "G-MTHHCWPVE2"
 };
 
 // Initialize Firebase
